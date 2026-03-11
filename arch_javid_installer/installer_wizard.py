3deb2ca7f2f26f6dcfbe70868f439e63eb0280ef
Javid Ahmed <javidahmed@icloud.com>
2026-03-11 20:18:04 +0000

    Refactor InstallerWizard to include keyboard options in pre-installation setup

--- arch_javid_installer/installer_wizard.py
+++ arch_javid_installer/installer_wizard.py
@@ -2,7 +2,7 @@
 
 from PySide6.QtWidgets import QWizard
 
-from arch_javid_installer.helpers import get_language_options, get_region_options
+from arch_javid_installer.helpers import get_keyboard_options, get_language_options, get_region_options
 from arch_javid_installer.models import RegionOptions
 from arch_javid_installer.pages import (
     DiskPage,
@@ -25,12 +25,25 @@ class InstallerWizard(QWizard):
 
         # Pre-installation
         _language_options = get_language_options()
-        _regions_dict = get_region_options()
+        _regions_options = get_region_options()
+        _keyboard_models, _keyboard_layouts_dict = get_keyboard_options()
 
         self.pages = {
             "welcome": WelcomePage(language_options=_language_options, default_locale="en_GB.UTF-8"),
-            "location": LocationPage(regions_dict=_regions_dict, default_region=RegionOptions.EUROPE),
-            "keyboard": KeyboardPage(),
+            "location": LocationPage(
+                regions_dict=_regions_options, default_region=RegionOptions.EUROPE, default_zone="London"
+            ),
+            "keyboard": KeyboardPage(
+                models=_keyboard_models,
+                layouts_dict=_keyboard_layouts_dict,
+                default_model=next(
+                    (model for model in _keyboard_models if model.model == "pc105"), _keyboard_models[0]
+                ),
+                default_layout=next(
+                    (layout for layout in _keyboard_layouts_dict.keys() if layout.layout == "gb"),
+                    next(iter(_keyboard_layouts_dict.keys())),
+                ),
+            ),
             "disk": DiskPage(),
             "user": UserPage(),
             "summary": SummaryPage(),
